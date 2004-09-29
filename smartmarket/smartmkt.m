f86a4dc46ea7b2c4a64e6b827f9fa3d4773c7c3b
Ray Zimmerman <rz10@cornell.edu>
2004-09-29 00:07:28 +0000

    Renamed reserves to imports.

--- smartmarket/smartmkt.m
+++ smartmarket/smartmkt.m
@@ -51,16 +51,16 @@ success = 0;
 [ng, np] = size(q);
 dispatch = zeros(ng, PENALTY);
 
-%% find reserve generators
-reserves = find(gen(:, GEN_STATUS) == -1);
+%% find import generators
+imports = find(gen(:, GEN_STATUS) == -1);
 
-if ~isempty(reserves)
-	%% modify p so that reserves come in at reservation price + gap
+if ~isempty(imports)
+	%% modify p so that imports come in at reservation price + gap
 	%% or, alternatively, max offered price + gap
 	%% and all actual offers above reservation price are kept out
 	
 	%% find max offer price less than max_p
-	gap = 5;        %% set the gap between reservation price and max reserve price
+	gap = 5;        %% set the gap between reservation price and max import price
 	on = find(gen(:, GEN_STATUS) > 0);  %% which generators are on?
 	on_p = p(on,:);                     %% get submatrix to do vector mods
 	in  = find(on_p <= max_p);          %% which blocks are in?
@@ -70,9 +70,9 @@ if ~isempty(reserves)
 										%% eliminated by reservation price + gap
 	p(on, :) = on_p;                    %% put back submatrix
 	
-	%% set price at which reserves come in
-	p(reserves, :) = ones(length(reserves), np) * (max_offered_p + gap);
-	% p(reserves, :) = ones(length(reserves), np) * (max_p + gap);
+	%% set price at which imports come in
+	p(imports, :) = ones(length(imports), np) * (max_offered_p + gap);
+	% p(imports, :) = ones(length(imports), np) * (max_p + gap);
 	
 	%% bump up max price
 	max_p = max_p + gap;
@@ -82,10 +82,10 @@ end
 %% eliminates offers (but not bids) above max_p
 [gen, genoffer] = off2case(gen, gencost, q, p, max_p);
 
-%% set reserve cost equal to reserve offers (replace zero placeholders)
-if ~isempty(reserves)
+%% set import cost equal to import offers (replace zero placeholders)
+if ~isempty(imports)
     tmp = min([ size(gencost, 2) size(genoffer, 2) ]);
-    gencost(reserves, 1:tmp) = genoffer(reserves, 1:tmp);
+    gencost(imports, 1:tmp) = genoffer(imports, 1:tmp);
 end
 
 %% make copy of initial bus voltages and generator outputs
@@ -111,7 +111,7 @@ while 1
     on = find(gen(:, GEN_STATUS) > 0);      %% which generators are on?
     gen_capacity    = sum(gen(on, PMAX));
     if gen_capacity < load_capacity * margin & ~withreserves
-        %%-----  insufficient generation, try again with reserves  -----
+        %%-----  insufficient generation, try again with imports  -----
         success = 0;
         if verbose
             fprintf('\nSMARTMARKET: insufficient generation');
@@ -131,11 +131,11 @@ while 1
     end
     
     %% should we retry?
-    if ~success & ~withreserves & ~isempty(reserves)
+    if ~success & ~withreserves & ~isempty(imports)
         if verbose
-            fprintf('\nSMARTMARKET: turning on reserve generators\n\n');
+            fprintf('\nSMARTMARKET: turning on import generators\n\n');
         end
-        gen(reserves, GEN_STATUS) = 2 * ones(size(reserves));
+        gen(imports, GEN_STATUS) = 2 * ones(size(imports));
         withreserves = 1;
     else
         break;
@@ -158,7 +158,7 @@ if success      %% OPF solved case fine
     end
     price = cp(:, 1);           %% need this for prices for gens that are shut down
     price(k) = sum( cq(k, :)' .* cp(k, :)' )' ./ sum( cq(k, :)' )';
-else        %% did not converge even with reserves
+else        %% did not converge even with imports
     quantity    = zeros(ng, 1);
     price       = max_p * ones(ng, 1);
 end
