b856d6d4e3db59d40089e42dc97deb74d32c9bfe
Ray Zimmerman <rz10@cornell.edu>
2004-12-15 22:46:32 +0000

    Added descriptions of columns to help section.

--- smartmarket/idx_disp.m
+++ smartmarket/idx_disp.m
@@ -1,6 +1,17 @@
 function [QUANTITY, PRICE, FCOST, VCOST, SCOST, PENALTY] = idx_disp
-%IDX_DISP   Defines variables for column indices to dispatch.
+%IDX_DISP   Defines constants for named column indices to dispatch matrix.
 %   [QUANTITY, PRICE, FCOST, VCOST, SCOST, PENALTY] = idx_disp
+%
+%   The index, name and meaning of each column of the dispatch matrix is given
+%   below:
+% 
+%   columns 1-6
+%    1  QUANTITY    quantity produced by generator in MW
+%    2  PRICE       market price for power produced by generator in $/MWh
+%    3  FCOST       fixed cost in $/MWh
+%    4  VCOST       variable cost in $/MWh
+%    5  SCOST       startup cost in $
+%    6  PENALTY     penalty cost in $ (not used)
 
 %   MATPOWER
 %   $Id$
@@ -14,6 +25,6 @@ PRICE           = 2;    %% market price for power produced by generator in $/MWh
 FCOST           = 3;    %% fixed cost in $/MWh
 VCOST           = 4;    %% variable cost in $/MWh
 SCOST           = 5;    %% startup cost in $
-PENALTY         = 6;    %% penalty cost in $
+PENALTY         = 6;    %% penalty cost in $ (not used)
 
 return;
