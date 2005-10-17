45c22bd2816b478ba841ea689d57913fbd290256
Ray Zimmerman <rz10@cornell.edu>
2005-10-17 22:21:01 +0000

    Rewrote make it generic.

--- smartmarket/auction.m
+++ smartmarket/auction.m
@@ -1,9 +1,15 @@
-function [cq, cp] = auction(bus, gen, gencost, q, p, max_p, auction_type, mpopt)
-%AUCTION  Clear auction based on OPF results.
-%   [cq, cp] = auction(bus, gen, gencost, q, p, max_p, auction_type, mpopt)
+function [co, cb] = auction(offers, bids, auction_type, limit_prc, gtee_prc)
+%AUCTION  Clear auction based on OPF results (qty's and lambdas).
+%   [co, cb] = auction(offers, bids, auction_type, limit_prc, gtee_prc)
 %   Clears a set of bids and offers based on the results of an OPF, where the
-%   pricing is adjusted for network losses and binding constraints. There are
-%   8 types of auctions implemented here, specified by auction_type.
+%   pricing is adjusted for network losses and binding constraints.
+%   The arguments offers and bids are structs with the following fields:
+%       qty       - m x n, offer/bid quantities, m offers/bids, n blocks
+%       prc       - m x n, offer/bid prices
+%       lam       - m x n, corresponding lambdas
+%       total_qty - m x 1, total quantity cleared for each offer/bid
+%
+%   There are 8 types of auctions implemented, specified by auction_type.
 %
 %      0 - discriminative pricing (price equal to offer or bid)
 %      1 - last accepted offer auction
@@ -16,229 +22,231 @@ function [cq, cp] = auction(bus, gen, gencost, q, p, max_p, auction_type, mpopt)
 %      7 - split the difference pricing (set by last accepted offer & bid)
 %      8 - LAO sets seller price, LAB sets buyer price
 %
-%   Cleared offer prices (but not bid prices) are clipped to max_p.
+%   Whether or not cleared offer (bid) prices are guaranteed to be greater
+%   (less) than or equal to the corresponding offer (bid) price is specified by
+%   a flag gtee_prc.offer (gtee_prc.bid). The default is value true.
+%   
+%   Offer/bid and cleared offer/bid min and max prices are specified in the
+%   limit_prc struct with the following fields:
+%       max_offer
+%       min_bid
+%       max_cleared_offer
+%       min_cleared_bid
+%   Offers (bids) above (below) max_offer (min_bid) are treated as withheld
+%   and cleared offer (bid) prices above (below) max_cleared_offer
+%   (min_cleared_bid) are clipped to max_cleared offer (min_cleared_bid) if
+%   given. All of these limit prices are ignored if the field is missing
+%   or is empty.
 
 %   MATPOWER
 %   $Id$
 %   by Ray Zimmerman, PSERC Cornell
-%   Copyright (c) 1996-2004 by Power System Engineering Research Center (PSERC)
+%   Copyright (c) 1996-2005 by Power System Engineering Research Center (PSERC)
 %   See http://www.pserc.cornell.edu/matpower/ for more info.
 
 %%-----  initialization  -----
-%% default parameters
-if nargin < 8
-    mpopt = mpoption;       %% use default options
-    if nargin < 7
-        auction_type = 1;       %% use 1st price by default
-    end
-end
+%% define named indices into data matrices
+[PQ, PV, REF, NONE, BUS_I, BUS_TYPE, PD, QD, GS, BS, BUS_AREA, VM, ...
+    VA, BASE_KV, ZONE, VMAX, VMIN, LAM_P, LAM_Q, MU_VMAX, MU_VMIN] = idx_bus;
+[GEN_BUS, PG, QG, QMAX, QMIN, VG, MBASE, GEN_STATUS, PMAX, PMIN, ...
+    MU_PMAX, MU_PMIN, MU_QMAX, MU_QMIN, PC1, PC2, QC1MIN, QC1MAX, ...
+    QC2MIN, QC2MAX, RAMP_AGC, RAMP_10, RAMP_30, RAMP_Q, APF] = idx_gen;
+[AREA_I, PRICE_REF_BUS] = idx_area;
 
-%% options
-verbose = mpopt(31);
+%% initialize some stuff
 if have_fcn('minopf')
     zero_tol = 1e-5;
 else
     zero_tol = 0.1;     %% fmincon is SO bad with prices that it is
-                        %% NOT recommended for use with auction.m
-end
+end                     %% NOT recommended for use with auction.m
 big_num = 1e6;
+if isempty(bids)
+    bids = struct(  'qty', [], ...
+                    'prc', [], ...
+                    'lam', [], ...
+                    'total_qty', [] );
+end
+if nargin < 4 | isempty(limit_prc)
+    limit_prc = struct( 'max_offer', [], 'min_bid', [], ...
+                        'max_cleared_offer', [], 'min_cleared_bid', [] );
+else
+    if ~isfield(limit_prc, 'max_offer'),         limit_prc.max_offer = [];         end
+    if ~isfield(limit_prc, 'min_bid'),           limit_prc.min_bid = [];           end
+    if ~isfield(limit_prc, 'max_cleared_offer'), limit_prc.max_cleared_offer = []; end
+    if ~isfield(limit_prc, 'min_cleared_bid'),   limit_prc.min_cleared_bid = [];   end
+end
 
-%% define named indices into data matrices
-[PQ, PV, REF, NONE, BUS_I, BUS_TYPE, PD, QD, GS, BS, BUS_AREA, VM, ...
-    VA, BASE_KV, ZONE, VMAX, VMIN, LAM_P, LAM_Q, MU_VMAX, MU_VMIN] = idx_bus;
-[GEN_BUS, PG, QG, QMAX, QMIN, VG, MBASE, GEN_STATUS, ...
-    PMAX, PMIN, MU_PMAX, MU_PMIN, MU_QMAX, MU_QMIN, QMAX2, QMIN2, ...
-    RAMP_AGC, RAMP_10, RAMP_30, RAMP_Q, APF] = idx_gen;
-[AREA_I, PRICE_REF_BUS] = idx_area;
+if nargin < 5 | isempty(gtee_prc)
+    gtee_prc = struct(  'offer', 1, 'bid', 1    );
+else
+    if ~isfield(gtee_prc, 'offer'), gtee_prc.offer = 1; end
+    if ~isfield(gtee_prc, 'bid'),   gtee_prc.bid = 1;   end
+end
 
-%% initialize some stuff
-[ng, np] = size(q);
-cq      = zeros(size(q));                   %% cleared quantity offers
-cp      = zeros(size(p));                   %% cleared price offers
-in      = find(gen(:, GEN_STATUS) >= 0);    %% which gens are in the game (non-reserve)
-                                            %% (NOTE: can include some that are off-line)
-G       = find( ~isload(gen(in, :)) );   	%% real generators
-L       = find(  isload(gen(in, :)) );   	%% dispatchable loads
-qin     = q(in, :);             %% reduce quantity offers ...
-pin     = p(in, :);             %% ... and price offers to those gens that are "in"
-cqin    = cq(in, :);
-cpin    = cp(in, :);
+[nro, nco] = size(offers.qty);
+[nrb, ncb] = size(bids.qty);
 
 %% determine cleared quantities
-Pg = gen(in, PG);           %% active generation from generators that are "in"
-Pg(L) = -Pg(L);             %% make quantity positive for loads
-accept = zeros(size(qin));
-for i = 1:length(in)        %% generator in(i)
-    for j = 1:np                %% block j
-        if qin(i, j)                    %% ignore zero quantity offers
+if isempty(limit_prc.max_offer)
+    [co.qty, o.on, o.off] = clear_qty(offers.qty, offers.total_qty);
+else
+    mask = offers.prc <= limit_prc.max_offer;
+    [co.qty, o.on, o.off] = clear_qty(offers.qty, offers.total_qty, mask);
+end
+if isempty(limit_prc.min_bid)
+    [cb.qty, b.on, b.off] = clear_qty(bids.qty, bids.total_qty);
+else
+    mask = bids.prc <= limit_prc.min_bid;
+    [cb.qty, b.on, b.off] = clear_qty(bids.qty, bids.total_qty, mask);
+end
+
+%% initialize cleared prices
+co.prc  = zeros(nro, nco);              %% cleared offer prices
+cb.prc  = zeros(nrb, ncb);              %% cleared bid prices
+
+%%-----  compute shift values to add to lam to get desired pricing  -----
+%% The locationally adjusted offer/bid price, when normalized to an arbitrary
+%% reference location where lambda is equal to ref_lam, is:
+%%      norm_prc = prc + (ref_lam - lam)
+%% Then we can define the difference between the normalized offer/bid prices
+%% and the ref_lam as:
+%%      diff = norm_prc - ref_lam = prc - lam
+%% This diff represents the gap between the marginal unit (setting lambda)
+%% and the offer/bid price in question.
+offer_diff = offers.prc - offers.lam;
+bid_diff   = bids.prc   - bids.lam;
+
+%% shift.LAO + lambda is equal to the last accepted offer
+shift.LAO = o.on .* offer_diff - o.off * big_num;
+shift.LAO( find(shift.LAO(:) > zero_tol) ) = -big_num;  %% don't let gens @ Pmin set price
+shift.LAO = max( shift.LAO(:) );
+
+%% shift.FRO + lambda is equal to the first rejected offer
+shift.FRO = o.off .* offer_diff + o.on * big_num;
+shift.FRO = min( shift.FRO(:) );
+
+if nrb
+    %% shift.LAB + lambda is equal to the last accepted bid
+    shift.LAB = b.on .* bid_diff + b.off * big_num;
+    shift.LAB = min( shift.LAB(:) );
+    
+    %% shift.FRB + lambda is equal to the first rejected bid
+    shift.FRB = b.off .* bid_diff - b.on * big_num;
+    shift.FRB = max( shift.FRB(:) );
+else
+    shift.LAB = big_num;
+    shift.FRB = shift.LAB;
+end
+
+%% cleared offer/bid prices for different auction types
+if auction_type == 0        %% discriminative
+    co.prc = offers.prc;
+    cb.prc = bids.prc;
+elseif auction_type == 1    %% LAO
+    co.prc = offers.lam + shift.LAO;
+    cb.prc = bids.lam   + shift.LAO;
+elseif auction_type == 2    %% FRO
+    co.prc = offers.lam + shift.FRO;
+    cb.prc = bids.lam   + shift.FRO;
+elseif auction_type == 3    %% LAB
+    co.prc = offers.lam + shift.LAB;
+    cb.prc = bids.lam   + shift.LAB;
+elseif auction_type == 4    %% FRB
+    co.prc = offers.lam + shift.FRB;
+    cb.prc = bids.lam   + shift.FRB;
+elseif auction_type == 5    %% 1st price
+    co.prc = offers.lam;
+    cb.prc = bids.lam;
+elseif auction_type == 6    %% 2nd price
+    if abs(shift.LAO) < zero_tol
+        co.prc = offers.lam + min(shift.FRO,shift.LAB);
+        cb.prc = bids.lam   + min(shift.FRO,shift.LAB);
+    else
+        co.prc = offers.lam + max(shift.LAO,shift.FRB);
+        cb.prc = bids.lam   + max(shift.LAO,shift.FRB);
+    end
+elseif auction_type == 7    %% split the difference
+    co.prc = offers.lam + (shift.LAO + shift.LAB) / 2;
+    cb.prc = bids.lam   + (shift.LAO + shift.LAB) / 2;
+elseif auction_type == 8    %% LAO seller, LAB buyer
+    co.prc = offers.lam + shift.LAO;
+    cb.prc = bids.lam   + shift.LAB;
+end
+
+%% guarantee that cleared offer prices are >= offers
+if gtee_prc.offer
+    clip = o.on .* (offers.prc - co.prc);
+    co.prc = co.prc + (clip > zero_tol) .* clip;
+end
+
+%% guarantee that cleared bid prices are >= bids
+if gtee_prc.bid
+    clip = b.on .* (bids.prc - cb.prc);
+    cb.prc = cb.prc + (clip < -zero_tol) .* clip;
+end
+
+%% clip cleared offer prices by limit_prc.max_cleared_offer
+if ~isempty(limit_prc.max_cleared_offer)
+    co.prc = co.prc + (co.prc > limit_prc.max_cleared_offer) .* ...
+        (limit_prc.max_cleared_offer - co.prc);
+end
+
+%% clip cleared bid prices by limit_prc.min_cleared_bid
+if ~isempty(limit_prc.min_cleared_bid)
+    cb.prc = cb.prc + (cb.prc < limit_prc.min_cleared_bid) .* ...
+        (limit_prc.min_cleared_bid - co.prc);
+end
+
+%% make prices uniform after clipping (except for discrim auction)
+%% since clipping may only affect a single block of a multi-block generator
+if auction_type ~= 0
+    %% equal to largest price in row
+    if nco > 1
+        co.prc = diag(max(co.prc')) * ones(nro,nco);
+    end
+    if ncb > 1
+        cb.prc = diag(min(cb.prc')) * ones(nrb,ncb);
+    end
+end
+
+return;
+
+
+function [cqty, on, off] = clear_qty(qty, total_cqty, mask)
+%CLEAR_QTY  Computed cleared offer/bid quantities from totals.
+%  Inputs:
+%   qty        - m x n, offer/bid quantities, m offers/bids, n blocks
+%   total_cqty - m x 1, total cleared quantity for each offer/bid
+%   mask       - m x n, boolean indicating which offers/bids are valid (not withheld)
+%  Outputs:
+%   cqty       - m x n, cleared offer/bid quantities, m offers/bids, n blocks
+%   on         - m x n, 1 if partially or fully accepted, 0 if rejected
+%   off        - m x n, 1 if rejected, 0 if partially or fully accepted
+
+[nr, nc] = size(qty);
+accept  = zeros(nr,nc);
+cqty    = zeros(nr,nc);
+for i = 1:nr            %% offer/bid i
+    for j = 1:nc        %% block j
+        if qty(i, j)    %% ignore zero quantity offers/bids
             %% compute fraction of the block accepted ...
-            accept(i, j) = (Pg(i) - sum(qin(i, 1:j-1))) / qin(i, j);
+            accept(i, j) = (total_cqty(i) - sum(qty(i, 1:j-1))) / qty(i, j);
             %% ... clipped to the range [0, 1]  (i.e. 0-100%)
             if accept(i, j) > 1
                 accept(i, j) = 1;
             elseif accept(i, j) < 1.0e-5
                 accept(i, j) = 0;
             end
-            cqin(i, j) = qin(i, j) * accept(i, j);
+            cqty(i, j) = qty(i, j) * accept(i, j);
         end
     end
 end
 
-offered = pin <= max_p;                         %% make sure we haven't
-accept(G, :) = offered(G, :) .* accept(G, :);   %% accepted any withheld offers
+if nargin == 3
+    accept = mask .* accept;
+end
+
 on  = (accept  > 0);
 off = (accept == 0);
-all_ones  = ones(size(pin));
-all_zeros = zeros(size(pin));
-
-%% get nodal marginal prices from OPF
-gbus    = gen(in, GEN_BUS);                     %% indices of buses w/gens
-lamP    = diag(bus(gbus, LAM_P)) * all_ones;    %% real power prices
-lamQ    = diag(bus(gbus, LAM_Q)) * all_ones;    %% reactive power prices
-
-%% compute fudge factor for lamP to include price of bundled reactive power
-nz          = find( gen(L, PG) );
-pf          = all_zeros;                        %% for loads Q = pf * P
-Qlim = 	(gen(L, QMIN) == 0) .* gen(L, QMAX) + ...
-		(gen(L, QMAX) == 0) .* gen(L, QMIN);
-pf(L) = Qlim ./ gen(L, PMIN);
-Qfudge      = all_zeros;
-Qfudge(L,:) = diag(pf(L)) * lamQ(L,:);
-
-%%-----  compute shift matrices to add to lamP to get desired pricing  -----
-%% If refP is the lambda P at an arbitrary reference bus, and normP is
-%% the set of offers/bids normalized to the refbus location with bids
-%% normalized to include bundled reactive power as well, then
-%%     gap = normP - refP
-%% In other words, (gap + refP) can be used to form the normal
-%% non-locational bid/offer stack. And since refP is just a constant
-%% it doesn't actually change the shape of the stacks.
-gap = pin - lamP - Qfudge;
-
-DISC_P  = gap;
-DISC_PQ = gap + Qfudge;
-
-LAO = on(G,:) .* gap(G,:) - off(G,:) * big_num;
-LAO( find(LAO(:) > zero_tol) ) = -big_num;  %% don't let gens @ Pmin set price
-LAO = max( LAO(:) ) * all_ones;
-
-FRO = off(G,:) .* gap(G,:) + on(G,:) * big_num;
-FRO = min( FRO(:) ) * all_ones;
-
-if ~isempty(L)
-    LAB_P = on(L,:) .* gap(L,:) + off(L,:) * big_num;
-    LAB_P = min( LAB_P(:) ) * all_ones;
-    
-    FRB_P = off(L,:) .* gap(L,:) - on(L,:) * big_num;
-    FRB_P = max( FRB_P(:) ) * all_ones;
-else
-    LAB_P = big_num * all_ones;
-    FRB_P = LAB_P;
-end
-LAB_PQ = LAB_P + Qfudge;
-FRB_PQ = FRB_P + Qfudge;
-
-%% generator and load price shifts for different auction types
-if auction_type == 0
-    G_shift = DISC_P;
-    L_shift = DISC_PQ;
-elseif auction_type == 1
-    G_shift = LAO;
-    L_shift = LAO + Qfudge;
-elseif auction_type == 2
-    G_shift = FRO;
-    L_shift = FRO + Qfudge;
-elseif auction_type == 3
-    G_shift = LAB_P;
-    L_shift = LAB_PQ;
-elseif auction_type == 4
-    G_shift = FRB_P;
-    L_shift = FRB_PQ;
-elseif auction_type == 5
-    G_shift = all_zeros;
-    L_shift = Qfudge;
-elseif auction_type == 6
-    if abs(LAO(1,1)) < zero_tol
-        G_shift = min(FRO(1,1),LAB_P(1,1)) * all_ones;
-        L_shift = min(FRO(1,1),LAB_P(1,1)) + Qfudge;
-    else
-        G_shift = max(LAO(1,1),FRB_P(1,1)) * all_ones;
-        L_shift = max(LAO(1,1),FRB_P(1,1)) + Qfudge;
-    end
-elseif auction_type == 7
-    G_shift = (LAO + LAB_P) / 2;
-    L_shift = (LAO + LAB_P) / 2 + Qfudge;
-elseif auction_type == 8
-    G_shift = LAO;
-    L_shift = LAB_PQ;
-end
-
-%% compute the prices
-cpin(G,:) = lamP(G,:) + G_shift(G,:);
-cpin(L,:) = lamP(L,:) + L_shift(L,:);
-
-%% clip cleared prices by offers and bids
-clip = on .* (pin - cpin);
-cpin(G,:) = cpin(G,:) + (clip(G,:) > zero_tol) .* clip(G,:);
-cpin(L,:) = cpin(L,:) + (clip(L,:) < -zero_tol) .* clip(L,:);
-
-%% clip cleared offer prices by max_p
-cpin(G,:) = cpin(G,:) + (cpin(G,:) > max_p) .* (max_p - cpin(G,:));
-
-%% make prices uniform after clipping (except for discrim auction)
-%% since clipping may only affect a single block of a multi-block generator
-if auction_type ~= 0 & np > 1
-    cpin(G,:) = diag(max(cpin(G,:)')) * all_ones(G,:);  %% equal to largest price in row
-    cpin(L,:) = diag(min(cpin(L,:)')) * all_ones(L,:);  %% equal to smallest price in row
-end
-
-cq(in, :) = cqin;
-cp(in, :) = cpin;
-
-%%-----  plot offers, adjusted offers, prices  -----
-if verbose
-    %% compute locational adjustment for generators
-    ref_p = max(lamP(:,1));
-    % ref_p = max(bus(:, LAM_P));
-    adjustment = ref_p - lamP(:,1);         %% locational price adjustment
-    
-    %% form 1-d vectors of all valid offers & corresponding adjustments, acceptance
-    i = find(qin & pin <= max_p);
-    qq = qin(i);                            %% quantity vector
-    pp = pin(i);                            %% price vector
-    adj = diag(adjustment) * all_ones;      %% adjustment matrix
-    adj = adj(i);                           %% adjustment vector
-    adj_pp = pp + adj;                      %% adjusted offer price vector
-    acc = accept(i);                        %% acceptance vector
-    final_p = cpin;
-    final_p = final_p(i);
-    
-    %% sort by adjusted price
-    [junk, k] = sort(adj_pp);
-    qq = qq(k);
-    pp = pp(k);
-    adj = adj(k);
-    adj_pp = adj_pp(k);
-    acc = acc(k);
-    final_p = final_p(k);
-
-    %% plot it
-    subplot(1,1,1);
-    xd = [0; cumsum(qq)];
-    [xs, ys1] = stairs(xd, [pp; pp(length(pp))]);
-    [xs, ys2] = stairs(xd, [adj_pp; adj_pp(length(adj_pp))]);
-    [xs, ys3] = stairs(xd, [acc; acc(length(acc))]);
-    [xs, ys4] = stairs(xd, [final_p; final_p(length(final_p))]);
-    [xs1, ys5] = stairs([0 sum(Pg) sum(qq)], [0 100 100]);
-    plot(xs, ys2, 'r--');       %% adjusted offers
-    axis([0 1.1*max(xd) 0 max([110, 1.1*max(adj_pp)])]);
-    hold on;
-    plot(xs, ys1, 'y-.');       %% unadjusted offers
-    plot(xs, 100*ys3, 'b:');    %% percent of block accepted
-    plot(xs, ys4, 'g');         %% price paid
-%   plot(xs1, ys5, 'b');
-    hold off;
-    drawnow;
-end
 
 return;
