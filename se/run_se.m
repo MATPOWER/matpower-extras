0d7516dfe27ef8b175b01122b4cc63e7bcd69776
Ray Zimmerman <rz10@cornell.edu>
2016-12-12 11:36:52 -0500

    Update Pg and Qg in solution using estimated values.

--- se/run_se.m
+++ se/run_se.m
@@ -1,4 +1,5 @@
-function [baseMVA, bus, gen, branch, success, et, z, z_est, error_sqrsum] = run_se(casename, measure, idx, sigma, type_initialguess, V0)
+function [baseMVA, bus, gen, branch, success, et, z, z_est, error_sqrsum] = ...
+    run_se(casename, measure, idx, sigma, type_initialguess, V0)
 %RUN_SE  Run state estimation.
 %   [INPUT PARAMETERS]
 %   measure: measurements
@@ -20,6 +21,11 @@ function [baseMVA, bus, gen, branch, success, et, z, z_est, error_sqrsum] = run_
 %   This file is part of MATPOWER.
 %   Covered by the 3-clause BSD License (see LICENSE file for details).
 %   See http://www.pserc.cornell.edu/matpower/ for more info.
+
+%% define named indices into data matrices
+[GEN_BUS, PG, QG, QMAX, QMIN, VG, MBASE, GEN_STATUS, PMAX, PMIN, ...
+    MU_PMAX, MU_PMIN, MU_QMAX, MU_QMIN, PC1, PC2, QC1MIN, QC1MAX, ...
+    QC2MIN, QC2MAX, RAMP_AGC, RAMP_10, RAMP_30, RAMP_Q, APF] = idx_gen;
 
 %% read data & convert to internal bus numbering
 [baseMVA, bus, gen, branch] = loadcase(casename);
@@ -43,7 +49,20 @@ end
 
 %% run state estimation
 t0 = clock;
-[V, success, iterNum, z, z_est, error_sqrsum] = doSE(baseMVA, bus, gen, branch, Ybus, Yf, Yt, V0, ref, pv, pq, measure, idx, sigma);
+[V, success, iterNum, z, z_est, error_sqrsum] = ...
+    doSE(baseMVA, bus, gen, branch, Ybus, Yf, Yt, V0, ref, pv, pq, measure, idx, sigma);
+
+% update Pg and Qg using estimated values
+if length(idx.idx_zPG) > 0
+    cnt = length(idx.idx_zPF) + length(idx.idx_zPT);
+    gen(idx.idx_zPG, PG) = z_est([cnt+1:cnt+length(idx.idx_zPG)]) * baseMVA;
+end
+if length(idx.idx_zQG) > 0
+    cnt = length(idx.idx_zPF) + length(idx.idx_zPT) + length(idx.idx_zPG) + ...
+            length(idx.idx_zVa) + length(idx.idx_zQF) + length(idx.idx_zQT);
+    gen(idx.idx_zQG, QG) = z_est([cnt+1:cnt+length(idx.idx_zQG)]) * baseMVA;
+end
+
 %% update data matrices with solution, ie, V
 % [bus, gen, branch] = updatepfsoln(baseMVA, bus, gen, branch, Ybus, V, ref, pv, pq);
 [bus, gen, branch] = pfsoln(baseMVA, bus, gen, branch, Ybus, Yf, Yt, V, ref, pv, pq);
