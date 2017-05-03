3dc2b580ff4c69020fe718d50ba988a42e07a92e
Ray Zimmerman <rz10@cornell.edu>
2017-05-03 09:26:54 -0400

    Add handling of bus voltage and gen Q limits to checklimits().

--- misc/checklimits.m
+++ misc/checklimits.m
@@ -96,6 +96,46 @@ Pv.I  = Pv.I(K);
 Pv.v  = Pv.v(k);
 Pv.V  = Pv.V(K);
 
+%%-----  generator reactive power  -----
+Qg = mpc.gen(:, QG);
+%% find Qmin and Qmax violations
+Qv.i = find(Qg < mpc.gen(:, QMIN) - tol & mpc.gen(:, GEN_STATUS) > 0);
+Qv.I = find(Qg > mpc.gen(:, QMAX) + tol & mpc.gen(:, GEN_STATUS) > 0);
+%% absolute gen limit violations
+Qv.v = mpc.gen(Qv.i, QMIN) - Qg(Qv.i);
+Qv.V = Qg(Qv.I) - mpc.gen(Qv.I, QMAX);
+%% percentage gen limit violations
+Qv.p = 100 * Qv.v ./ max(abs(mpc.gen(Qv.i, QMIN)), abs(mpc.gen(Qv.i, QMAX)));
+Qv.P = 100 * Qv.V ./ max(abs(mpc.gen(Qv.I, QMIN)), abs(mpc.gen(Qv.I, QMAX)));
+%% sort by percentage violation
+[Qv.p, k] = sort(Qv.p, 'descend');
+[Qv.P, K] = sort(Qv.P, 'descend');
+%% reorder indices, absolute violations
+Qv.i  = Qv.i(k);
+Qv.I  = Qv.I(K);
+Qv.v  = Qv.v(k);
+Qv.V  = Qv.V(K);
+
+%%-----  bus voltage magnitudes  -----
+Vm = mpc.bus(:, VM);
+%% find Vmin and Vmax violations
+Vv.i = find(Vm < mpc.bus(:, VMIN) - tol);
+Vv.I = find(Vm > mpc.bus(:, VMAX) + tol);
+%% absolute voltage limit violations
+Vv.v = mpc.bus(Vv.i, VMIN) - Vm(Vv.i);
+Vv.V = Vm(Vv.I) - mpc.bus(Vv.I, VMAX);
+%% percentage voltage limit violations
+Vv.p = 100 * Vv.v ./ max(abs(mpc.bus(Vv.i, VMIN)), abs(mpc.bus(Vv.i, VMAX)));
+Vv.P = 100 * Vv.V ./ max(abs(mpc.bus(Vv.I, VMIN)), abs(mpc.bus(Vv.I, VMAX)));
+%% sort by percentage violation
+[Vv.p, k] = sort(Vv.p, 'descend');
+[Vv.P, K] = sort(Vv.P, 'descend');
+%% reorder indices, absolute violations
+Vv.i  = Vv.i(k);
+Vv.I  = Vv.I(K);
+Vv.v  = Vv.v(k);
+Vv.V  = Vv.V(K);
+
 if ~quiet
     fprintf('\n');
     if isempty(Fv.ic)
@@ -151,7 +191,7 @@ if ~quiet
     end
 
     fprintf('\n');
-    fprintf('Generator Limit Violations\n');
+    fprintf('Generator Active Limit Violations\n');
     if isempty(Pv.i)
         fprintf('No Pmin Violations\n');
     else
@@ -183,6 +223,78 @@ if ~quiet
                 mpc.gen(Pv.I(k), PMAX), ...
                 Pv.V(k), ...
                 Pv.P(k) ...
+            );
+        end
+    end
+
+    fprintf('\n');
+    fprintf('Generator Reactive Limit Violations\n');
+    if isempty(Qv.i)
+        fprintf('No Qmin Violations\n');
+    else
+        fprintf('Qmin Violations\n');
+        fprintf('  gen       bus        Qmin         Qg         Qmax     violation  %% violation\n');
+        fprintf('--------  --------  ----------  ----------  ----------  ----------  ----------\n');
+        for k = 1:length(Qv.i);
+            fprintf('%7d  %8d  %10.1f  %10.1f  %10.1f  %10.2f  %10.1f\n', Qv.i(k), ...
+                mpc.gen(Qv.i(k), GEN_BUS), ...
+                mpc.gen(Qv.i(k), QMIN), ...
+                Qg(Qv.i(k)), ...
+                mpc.gen(Qv.i(k), QMAX), ...
+                Qv.v(k), ...
+                Qv.p(k) ...
+            );
+        end
+    end
+    if isempty(Qv.I)
+        fprintf('No Qmax Violations\n');
+    else
+        fprintf('Qmax Violations\n');
+        fprintf('  gen       bus        Qmin         Qg         Qmax     violation  %% violation\n');
+        fprintf('--------  --------  ----------  ----------  ----------  ----------  ----------\n');
+        for k = 1:length(Qv.I);
+            fprintf('%7d  %8d  %10.1f  %10.1f  %10.1f  %10.2f  %10.1f\n', Qv.I(k), ...
+                mpc.gen(Qv.I(k), GEN_BUS), ...
+                mpc.gen(Qv.I(k), QMIN), ...
+                Qg(Qv.I(k)), ...
+                mpc.gen(Qv.I(k), QMAX), ...
+                Qv.V(k), ...
+                Qv.P(k) ...
+            );
+        end
+    end
+
+    fprintf('\n');
+    fprintf('Bus Voltage Limit Violations\n');
+    if isempty(Vv.i)
+        fprintf('No Vmin Violations\n');
+    else
+        fprintf('Vmin Violations\n');
+        fprintf('   bus        Vmin         Vm         Vmax     violation  %% violation\n');
+        fprintf(' --------  ----------  ----------  ----------  ----------  ----------\n');
+        for k = 1:length(Vv.i);
+            fprintf('%8d  %10.2f  %10.2f  %10.2f  %10.2f  %10.2f\n', Vv.i(k), ...
+                mpc.bus(Vv.i(k), VMIN), ...
+                Vm(Vv.i(k)), ...
+                mpc.bus(Vv.i(k), VMAX), ...
+                Vv.v(k), ...
+                Vv.p(k) ...
+            );
+        end
+    end
+    if isempty(Vv.I)
+        fprintf('No Vmax Violations\n');
+    else
+        fprintf('Vmax Violations\n');
+        fprintf('   bus        Vmin         Vm         Vmax     violation  %% violation\n');
+        fprintf(' --------  ----------  ----------  ----------  ----------  ----------\n');
+        for k = 1:length(Vv.I);
+            fprintf('%8d  %10.2f  %10.2f  %10.2f  %10.2f  %10.2f\n', Vv.I(k), ...
+                mpc.bus(Vv.I(k), VMIN), ...
+                Vm(Vv.I(k)), ...
+                mpc.bus(Vv.I(k), VMAX), ...
+                Vv.V(k), ...
+                Vv.P(k) ...
             );
         end
     end
